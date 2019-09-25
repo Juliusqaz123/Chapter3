599530805a9d4085c241fad05ad6a50f6521e67a
Julius <juliusblusevicius20@gmail.com>
2019-09-25 14:20:17 +0300

    Using a key container for storing an asymetric key

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -8,30 +8,18 @@ namespace Chapter3
     {
         static void Main(string[] args)
         {
-            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
-            string publicKeyXML = rsa.ToXmlString(false);
-            string privateKeyXML = rsa.ToXmlString(true);
-
             UnicodeEncoding ByteConverter = new UnicodeEncoding();
             byte[] dataToEncrypt = ByteConverter.GetBytes("My Secret Data!");
 
+            string containerName = "SecretContainer";
+            CspParameters csp = new CspParameters() { KeyContainerName = containerName };
             byte[] encryptedData;
-            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider(csp))
             {
-                RSA.FromXmlString(publicKeyXML);
                 encryptedData = RSA.Encrypt(dataToEncrypt, false);
             }
 
-            byte[] decryptedData;
-            using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
-            {
-                RSA.FromXmlString(privateKeyXML);
-                decryptedData = RSA.Decrypt(encryptedData, false);
-            }
-
-            string decryptedString = ByteConverter.GetString(decryptedData);
-            Console.WriteLine(decryptedString);
-
+            Console.WriteLine(encryptedData);
             Console.ReadLine();
         }
     }
