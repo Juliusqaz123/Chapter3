20eaa4445bbfddbb788c4f35de5bf42abddafe19
Julius <juliusblusevicius20@gmail.com>
2019-09-25 15:41:43 +0300

    Applying the DebuggerDisplayAttribute

---
+++ Chapter3/Person.cs
@@ -0,0 +1,11 @@
+﻿using System.Diagnostics;
+
+namespace Chapter3
+{
+    [DebuggerDisplay("Name = {FirstName} {LastName}")]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -7,15 +7,7 @@ namespace Chapter3
     {
         public static void Main()
         {
-#pragma warning disable 0162, 0168
-            int i;
-#pragma warning restore 0162
-            while (false)
-            {
-                Console.WriteLine("Unreachable code");
-            }
-#pragma warning restore
-            Console.ReadLine();
+            //-
         }
 
         public void SomeMethod()
