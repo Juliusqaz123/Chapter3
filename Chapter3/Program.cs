aab5ac18db1918665362af1e7e315fee7753e182
Julius <juliusblusevicius20@gmail.com>
2019-09-25 14:57:17 +0300

    Imperative CAS

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -1,38 +1,27 @@
 ﻿using System;
-using System.Linq;
-using System.Security.Cryptography;
+using System.Security;
 using System.Security.Permissions;
-using System.Text;
 
 namespace Chapter3
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            UnicodeEncoding byteConverter = new UnicodeEncoding();
-            SHA256 sha256 = SHA256.Create();
-
-            string data = "A paragraph of text";
-            byte[] hashA = sha256.ComputeHash(byteConverter.GetBytes(data));
-
-            data = "A paragraph of changed text";
-            byte[] hashB = sha256.ComputeHash(byteConverter.GetBytes(data));
-
-            data = "A paragraph of text";
-            byte[] hashC = sha256.ComputeHash(byteConverter.GetBytes(data));
-
-            Console.WriteLine(hashA.SequenceEqual(hashB));
-            Console.WriteLine(hashA.SequenceEqual(hashC));
+            FileIOPermission f = new FileIOPermission(PermissionState.None)
+            {
+                AllLocalFiles = FileIOPermissionAccess.Read
+            };
+            try
+            {
+                f.Demand();
+            }
+            catch (SecurityException s)
+            {
+                Console.WriteLine(s.Message);
+            }
 
             Console.ReadLine();
-        }
-
-        [FileIOPermission(SecurityAction.Demand,
-            AllLocalFiles = FileIOPermissionAccess.Read)]
-        public void DeclarativeCAS()
-        {
-            // Method body
         }
     }
 }
