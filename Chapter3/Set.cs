22e25373eb4eaffdcfa2d716b7a9e23971ff97ad
Julius <juliusblusevicius20@gmail.com>
2019-09-25 14:29:33 +0300

    A set implementation that uses hashing

--- Chapter3/Set.cs
+++ Chapter3/Set.cs
@@ -4,19 +4,37 @@ namespace Chapter3
 {
     class Set<T>
     {
-        private readonly List<T> list = new List<T>();
+        private readonly List<T>[] buckets = new List<T>[100];
 
         public void Insert(T item)
         {
-            if (!Contains(item))
-                list.Add(item);
+            int bucket = GetBucket(item.GetHashCode());
+            if (Contains(item, bucket))
+                return;
+            if (buckets[bucket] == null)
+                buckets[bucket] = new List<T>();
+            buckets[bucket].Add(item);
         }
 
         public bool Contains(T item)
         {
-            foreach (T member in list)
-                if (member.Equals(item))
-                    return true;
+            return Contains(item, GetBucket(item.GetHashCode()));
+        }
+
+        private int GetBucket(int hashcode)
+        {
+            unchecked
+            {
+                return (int)((uint)hashcode % (uint)buckets.Length);
+            }
+        }
+
+        private bool Contains(T item, int bucket)
+        {
+            if (buckets[bucket] != null)
+                foreach (T member in buckets[bucket])
+                    if (member.Equals(item))
+                        return true;
             return false;
         }
     }
